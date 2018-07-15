71b93992689ba93b038e865f77eb67f841bdfff4
Ezra Singh <singhezra@gmail.com>
2018-07-15 18:15:00 -0400

    Simplified Apollo client

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,17 +1,12 @@
 import 'cross-fetch/polyfill'
-import ApolloClient from 'apollo-boost'
+import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-
-const endpoint = 
-    process.env.NODE_ENV !== 'production' ?
-        'http://localhost:5001/api' :
-        '/api'
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: createHttpLink({
-        uri: endpoint
+        uri: "/api"
     })
 })
 
