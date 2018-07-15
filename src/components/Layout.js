8357b9ef341627e7f1b358a5427b777bb9c55b54
Ezra Singh <singhezra@gmail.com>
2018-07-15 15:58:19 -0400

    Optimized state mgmt and API calls with a separate presentational layout

---
+++ src/components/Layout.js
@@ -0,0 +1,26 @@
+import Search from './Search'
+import Status from './Status'
+
+const Layout = ({ state, actions, loading, anagrams }) => {
+    return(
+        <section className="hero is-fullheight">
+            <div className="hero-body">
+                <div className="container is-pulled-left">
+                <h1 className="title">
+                        The Anagramer
+                    </h1>
+                    <h2 className="subtitle">
+                        Find anagrams ... like really fast.
+                    </h2>
+                    <Search 
+                        query={state.query} 
+                        handleChange={actions.update}
+                    />
+                </div>
+                <div className="container is-pulled-right">
+                    <Status anagrams={anagrams}/>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/containers/UI.js
+++ src/containers/UI.js
@@ -1,7 +1,9 @@
 import { Component } from 'react'
-// Components
-import Search from '../components/Search'
-import Status from '../components/Status'
+// GraphQL bindings
+import { Query } from 'react-apollo'
+import getAnagrams from '../graphql/queries.gql'
+//Components
+import Layout from '../components/Layout'
 
 class UI extends Component{
     state = { query: "" }
@@ -9,27 +11,26 @@ class UI extends Component{
         this.setState({ query: event.target.value })
     }
     render(){
-        const { query } = this.state
+        const actions = {
+            update: this.updateQuery.bind(this)
+        }
         return(
-            <section className="hero is-fullheight">
-                <div className="hero-body">
-                    <div className="container is-pulled-left">
-                    <h1 className="title">
-                            The Anagramer
-                        </h1>
-                        <h2 className="subtitle">
-                            Find anagrams ... like really fast.
-                        </h2>
-                        <Search 
-                            query={query} 
-                            handleChange={this.updateQuery.bind(this)}
-                        />
-                    </div>
-                    <div className="container is-pulled-right">
-                    <Status anagrams={anagrams}/>
-                    </div>
-                </div>
-            </section>
+            <Query 
+                query={getAnagrams} 
+                variables={{ word: this.state.query }}
+            >
+                {( { loading, error, data } ) => {
+                    if(error){
+                        console.error(error)
+                    }
+                    return <Layout
+                                loading={loading} 
+                                anagrams={data}
+                                actions={actions}
+                                state={this.state}
+                            />
+                }}
+            </Query>
         )
     }
 }
