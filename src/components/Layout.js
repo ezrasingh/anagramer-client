f24094138fcee08b7a06c307de342067dc410515
Ezra Singh <singhezra@gmail.com>
2018-07-15 18:24:36 -0400

    Updated prop assignments and and spaz to background

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,26 @@
 import Search from './Search'
 import Status from './Status'
 
-const Layout = ({ state, actions, loading, anagrams }) => {
+const Layout = ({ state, update, loading, anagrams }) => {
     return(
-        <section className="hero is-fullheight">
+        <section className="hero is-fullheight is-bold is-warning">
             <div className="hero-body">
                 <div className="container is-pulled-left">
                 <h1 className="title">
                         The Anagramer
                     </h1>
                     <h2 className="subtitle">
-                        Find anagrams ... like really fast.
+                        Find anagrams ... like really fast :)
                     </h2>
                     <Search 
-                        query={state.query} 
-                        handleChange={actions.update}
+                        query={state.word} 
+                        handleChange={update}
                     />
+                    <p>
+                        <em>
+                            Lost? Try <strong>team</strong> or <strong>aamrl</strong>.
+                        </em>
+                    </p>
                 </div>
                 <div className="container is-pulled-right">
                     <Status anagrams={anagrams}/>
