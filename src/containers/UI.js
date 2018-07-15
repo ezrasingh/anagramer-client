2e14bc6389882e4aafdf9c8a9a291d26bdfc745d
Ezra Singh <singhezra@gmail.com>
2018-07-15 15:41:53 -0400

    Structured for Apollo connection

--- src/containers/UI.js
+++ src/containers/UI.js
@@ -1,47 +1,37 @@
-import _ from 'lodash'
-import { connect } from 'react-redux'
-import { updateQuery } from '../actions'
+import { Component } from 'react'
 // Components
 import Search from '../components/Search'
 import Status from '../components/Status'
 
-const UI = ({ query, anagrams, update }) => {
-    return(
-        <section className="hero is-fullheight">
-            <div className="hero-body">
-                <div className="container is-pulled-left">
+class UI extends Component{
+    state = { query: "" }
+    updateQuery(event){
+        this.setState({ query: event.target.value })
+    }
+    render(){
+        const { query } = this.state
+        return(
+            <section className="hero is-fullheight">
+                <div className="hero-body">
+                    <div className="container is-pulled-left">
                     <h1 className="title">
-                        The Anagramer
-                    </h1>
-                    <h2 className="subtitle">
-                        Find anagrams ... like really fast.
-                    </h2>
-                    <Search 
-                        query={query} 
-                        handleChange={update}
-                    />
-                </div>
-                <div className="container is-pulled-right">
+                            The Anagramer
+                        </h1>
+                        <h2 className="subtitle">
+                            Find anagrams ... like really fast.
+                        </h2>
+                        <Search 
+                            query={query} 
+                            handleChange={this.updateQuery.bind(this)}
+                        />
+                    </div>
+                    <div className="container is-pulled-right">
                     <Status anagrams={anagrams}/>
+                    </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    }
 }
 
-const mapStateToProps = state => {
-    return { ...state }
-}
-
-const mapDispatchToProps = dispatch => {
-    return { 
-        update: (event) => {
-            _.flowRight(
-                dispatch,
-                updateQuery
-            )(event.target.value)
-        }
-     }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(UI)
+export default UI
