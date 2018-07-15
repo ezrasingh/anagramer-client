6aaecf76eb493d44e66c78a34617baf25f6543c7
Ezra Singh <ezra@lionsolutions.tech>
2018-07-15 15:17:44 -0400

    Application global reducer

---
+++ src/app.js
@@ -0,0 +1,14 @@
+/** Application Reducer */
+
+const DEFAULT_STATE = {
+    query: '',
+    anagrams: []
+}
+
+export default (state=DEFAULT_STATE, action) => {
+    switch(action.type){
+        case 'UPDATE_QUERY':
+        default:
+            return state
+    }
+}
